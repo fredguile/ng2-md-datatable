1368f6010ca574c299455ab1999ebed32e38d6c4
Frederic Ghilini <frederic@coffeecircle.com>
2017-06-07 17:47:22 +0200

    Fixing build

--- src/lib/md-datatable.component.ts
+++ src/lib/md-datatable.component.ts
@@ -7,6 +7,7 @@ import {
   ContentChild,
   ContentChildren,
   QueryList,
+  forwardRef,
 } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
@@ -54,7 +55,7 @@ export class MdDataTableComponent extends BaseComponent implements AfterContentI
   @Output() sortChange: EventEmitter<IDatatableSortEvent> =
     new EventEmitter<IDatatableSortEvent>(false);
 
-  @ContentChild(MdDataTableHeaderComponent) headerCmp: MdDataTableHeaderComponent;
+  @ContentChild(forwardRef(() => MdDataTableHeaderComponent)) headerCmp: MdDataTableHeaderComponent;
   @ContentChildren(MdDataTableRowComponent) rowsCmp: QueryList<MdDataTableRowComponent>;
 
   id = `md-datatable-${instanceId++}`;
